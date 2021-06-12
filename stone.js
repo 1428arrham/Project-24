5978adf2a145e33acd7f9dffdaf96262a66b8768
1428arrham <76648053+1428arrham@users.noreply.github.com>
2021-06-12 13:09:49 +0530

    Add files via upload

---
+++ stone.js
@@ -0,0 +1,30 @@
+class Stone{
+	constructor(x,y,width,height)
+
+	{    var options={
+		'density':12,
+		'friction': 0.9,
+		'restitution':0.8
+	  }
+		this.height = height;
+		this.width = width;
+		this.body=Bodies.rectangle(x,y,width,height, options)
+		World.add(world, this.body);
+
+	}
+	display()
+	{
+			var pos = this.body.position;		
+			var angle = this.body.angle;
+            push();
+			translate(pos.x,pos.y);
+			rectMode(CENTER);
+            rotate(angle);
+			strokeWeight(4);
+			stroke("black");
+			fill("black");
+            rect(0,0,this.width,this.height);
+			pop();
+	}
+
+}
